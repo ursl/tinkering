6479850d9c9891f3952330c6e16e3b748f2be5d7
Urs Langenegger <urslangenegger@gmail.com>
2024-12-02 22:21:37 +0100

    initial typescript version

---
+++ ts/trap.ts
@@ -0,0 +1,31 @@
+// ----------------------------------------------------------------------
+// -- Program v1 for trap, based on single server motor and bump sensor
+// -- 2024/12/02
+// ----------------------------------------------------------------------
+
+// -- reset 
+input.onButtonPressed(Button.A, function () {
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 40)
+    basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
+})
+
+// -- both buttons do a reset
+input.onButtonPressed(Button.B, function () {
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 40)
+    basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
+})
+
+// -- start of main program
+kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 40)
+basic.showIcon(IconNames.Yes)
+basic.clearScreen()
+basic.forever(function () {
+    if (pins.analogReadPin(AnalogReadWritePin.P3) < 100) {
+        kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 130)
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.clearScreen()
+    }
+})
